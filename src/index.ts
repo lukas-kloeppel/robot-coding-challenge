188cc2bc26394ba570762509ca963aa51fba0f58
Lukas Kloeppel <lkloeppel@deloitte.de>
2019-05-25 14:51:41 +0200

    feat(setup): Setup of application with nodejs, webpack 4 and typescript

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+console.log('Server started');
+
+/**
+ * close all open connections and save game
+ */
+function gracefulShutdown(): void {
+  // Possibly close open connections /  save game result or do other actions
+  console.info('Shutting down server');
+  process.exit();
+
+}
+
+// listen for TERM signal .e.g. kill
+process.on('SIGTERM', gracefulShutdown);
+
+// listen for INT signal e.g. Ctrl-C
+process.on('SIGINT', gracefulShutdown);
+
+// module.exports = app;
